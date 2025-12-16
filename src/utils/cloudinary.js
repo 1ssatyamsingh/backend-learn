7fa51b01d188a580487b330499f810640c002296
Satyam Singh <1ssatyamsinghs@gmail.com>
2025-12-16 14:00:07 +0530

    fixing one error in cloudinary util

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -16,6 +16,8 @@ const uploadOnCloudinary = async (localFilePath) => {
     });
     // file has been uploaded successfully
     console.log('File uploaded to Cloudinary successfully:', response.url);
+
+    fs.unlinkSync(localFilePath);//remove the locally saved temporary file
     return response;
   } catch (error) {
     fs.unlinkSync(localFilePath);//remove the locally saved temporary file as the upload failed
